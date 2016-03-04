fd09633bb9fa0137884c50ae63c8eaf7546ae05c
Jhonnys López <jhonnys.lopez@softwaresci.com>
2016-03-04 16:25:16 -0500

    Use DI and MediatR on HomeController

---
+++ src/ContosoUniversity/Features/Home/About.cs
@@ -0,0 +1,44 @@
+﻿namespace ContosoUniversity.Features.Home
+{
+    using ContosoUniversity.DAL;
+    using ContosoUniversity.ViewModels;
+    using MediatR;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class About
+    {
+        public class Query : IRequest<IEnumerable<EnrollmentDateGroup>> { }
+
+        public class Handler : IRequestHandler<Query, IEnumerable<EnrollmentDateGroup>>
+        {
+            private readonly SchoolContext _dbContext;
+
+            public Handler(SchoolContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            public IEnumerable<EnrollmentDateGroup> Handle(Query message)
+            {
+                // Commenting out LINQ to show how to do the same thing in SQL.
+                //IQueryable<EnrollmentDateGroup> = from student in db.Students
+                //           group student by student.EnrollmentDate into dateGroup
+                //           select new EnrollmentDateGroup()
+                //           {
+                //               EnrollmentDate = dateGroup.Key,
+                //               StudentCount = dateGroup.Count()
+                //           };
+
+                // SQL version of the above LINQ code.
+                string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+                    + "FROM Person "
+                    + "WHERE Discriminator = 'Student' "
+                    + "GROUP BY EnrollmentDate";
+                IEnumerable<EnrollmentDateGroup> data = _dbContext.Database.SqlQuery<EnrollmentDateGroup>(query);
+
+                return data.ToList();
+            }
+        }
+    }
+}
