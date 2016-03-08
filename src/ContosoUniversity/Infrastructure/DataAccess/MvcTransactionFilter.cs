c13e6bc0ff818f814a7cd2e39def4c48cd750e03
Jhonnys López <jhonnys.lopez@softwaresci.com>
2016-03-08 14:25:00 -0500

    Use DependencyResolver MvcTransactionFilter

--- src/ContosoUniversity/Infrastructure/DataAccess/MvcTransactionFilter.cs
+++ src/ContosoUniversity/Infrastructure/DataAccess/MvcTransactionFilter.cs
@@ -1,22 +1,23 @@
 ﻿namespace ContosoUniversity.Infrastructure.DataAccess
 {
-    using System.Web.Mvc;
-    using App_Start;
     using DAL;
+    using System.Web.Mvc;
 
     public class MvcTransactionFilter : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             // Logger.Instance.Verbose("MvcTransactionFilter::OnActionExecuting");
-            var context = StructuremapMvc.ParentScope.CurrentNestedContainer.GetInstance<SchoolContext>();
+            // var context = StructuremapMvc.ParentScope.CurrentNestedContainer.GetInstance<SchoolContext>();
+            var context = DependencyResolver.Current.GetService<SchoolContext>();
             context.BeginTransaction();
         }
 
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
             // Logger.Instance.Verbose("MvcTransactionFilter::OnActionExecuted");
-            var instance = StructuremapMvc.ParentScope.CurrentNestedContainer.GetInstance<SchoolContext>();
+            // var instance = StructuremapMvc.ParentScope.CurrentNestedContainer.GetInstance<SchoolContext>();
+            var instance = DependencyResolver.Current.GetService<SchoolContext>();
             instance.CloseTransaction(filterContext.Exception);
         }
     }
